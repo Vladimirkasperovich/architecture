99ae32845ed9ce2c262b6c5e537840291b1e45ee
vovak <vova.kasper.boy@gmail.com>
2026-01-20 13:45:59 +0300

    feat: make responsive catalog

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -1,21 +1,35 @@
 import CatalogCover from '../../assets/catalog-cover.jpg'
 import s from './Catalog.module.css'
+import {useState} from "react";
 
 export const Catalog = () => {
+    const [inputValue, setInputValue] = useState('')
+    const [isChecked, setIsChecked] = useState(false)
+    const handleChange = (e) => setInputValue(e.target.value)
+    const handleCheck = (e) => setIsChecked(e.target.checked)
+    const onSubmit = (e) => {
+        e.preventDefault()
+    }
+
     return (
         <section className={s.catalog}>
+            <h1 className={s.catalog__title}>Get a catalog of our projects</h1>
             <div className={s.catalog__block}>
-                <div>
-                    <h1 className={s.catalog__title}>Get a catalog of our projects</h1>
-                    <div className={s.catalog__cover}>
-                        <img src={CatalogCover} alt="Catalog cover"/>
-                    </div>
+                <div className={s.catalog__cover}>
+                    <img src={CatalogCover} alt="Catalog cover"/>
                 </div>
-                <form className={s.form}>
+                <form className={s.form} onSubmit={onSubmit}>
                     <h2 className={s.form__title}>Leave your email to receive
                         our project catalog</h2>
                     <h3 className={s.form__text}>The catalog will help you choose a style and plan your layout.</h3>
-                    <input type="email" className={s.form__input} placeholder="Enter your email"/>
+                    <input type="email" onChange={handleChange} value={inputValue} className={s.form__input}
+                           placeholder="Enter your email"/>
+                    <label htmlFor="checkbox" className={s.catalog__formLabel}>
+                        <input type="checkbox" id="checkbox" onChange={handleCheck} checked={isChecked}
+                               className={s.catalog__formCheckbox}/>
+                        I agree to the processing of personal data
+                    </label>
+                    <button type='submit' className={s.catalog__formBtn}>Get the catalog</button>
                 </form>
             </div>
         </section>

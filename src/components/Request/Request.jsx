df59e35f2a0e32714f8caa2116a8053f70f08781
vovak <vova.kasper.boy@gmail.com>
2026-01-22 12:59:29 +0300

    feat: finished Request.jsx

--- src/components/Request/Request.jsx
+++ src/components/Request/Request.jsx
@@ -1,14 +1,19 @@
 import s from './Request.module.css'
+import {useState} from "react";
 
 export const Request = () => {
+    const [inputValue, setInputValue] = useState("")
+    const handleChange = (event) => setInputValue(event.target.value)
+    const onSubmit = (event) => event.preventDefault()
     return (
         <section className={s.request}>
             <div className={s.request__container}>
-                <form className={s.request__form}>
+                <form className={s.request__form} onSubmit={onSubmit}>
                     <h1 className={s.request__title}>Submit a request and we will assist you with your project</h1>
                     <div className={s.request__form__container}>
-                        <input type="email" placeholder="Enter your email" className={s.request__form__input}/>
-                        <button className={s.request__form__btn}>Submit a request</button>
+                        <input type="email" placeholder="Enter your email" className={s.request__form__input}
+                               value={inputValue} onChange={handleChange}/>
+                        <button type="submit" className={s.request__form__btn}>Submit a request</button>
                     </div>
                 </form>
             </div>
